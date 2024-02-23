88612ec32068a265440b6a0ddbfd52d58e20c54b
Shreya <s.shreyamarch2005@gmail.com>
2024-02-23 13:02:37 +0530

    prepare base model added

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -6,13 +6,6 @@ from cnnClassifier.utils.common import get_size
 from cnnClassifier.entity.config_entity import DataIngestionConfig
 
 
-import os
-import zipfile
-import gdown
-from cnnClassifier import logger
-from cnnClassifier.utils.common import get_size
-from cnnClassifier.entity.config_entity import DataIngestionConfig
-
 
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
